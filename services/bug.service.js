e08d87cd4cf88cb928d7a29ebd33ae6db226c715
Yonatan Hershko <yonher8@gmail.com>
2024-07-01 17:18:47 +0300

    connect backend

--- services/bug.service.js
+++ services/bug.service.js
@@ -2,6 +2,8 @@
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 
+
+const BASE_URL = '/api/bug/'
 const STORAGE_KEY = 'bugDB'
 
 _createBugs()
@@ -15,25 +17,39 @@ export const bugService = {
 
 
 function query() {
-    return storageService.query(STORAGE_KEY)
+    // return storageService.query(STORAGE_KEY)
+    return axios.get(BASE_URL)
+        .then(res => res.data)
+        .catch(err => console.log(err))
+
 }
+
+
 function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
+    return axios.get(BASE_URL + bugId)
+        .then(res => res.data)
+        // .then(bug => _setNextPrevbugId(bug))
 }
+
+// function getById(bugId) {
+//     return storageService.get(STORAGE_KEY, bugId)
+// }
+
 
 function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+    return axios.get(BASE_URL + bugId + '/remove')
+    .then(res => res.data)
 }
+
+
+
 
 function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
+    const url = BASE_URL + 'save'
+    let queryParams = `?title=${bug.title}&severity=${bug.severity}`
+    if (bug._id) queryParams += `&_id=${bug._id}`
+    return axios.get(url + queryParams).then(res => res.data)
 }
-
-
 
 
 function _createBugs() {
@@ -43,25 +59,25 @@ function _createBugs() {
             {
                 title: "Infinite Loop Detected",
                 severity: 4,
-                description:"des",
+                description: "des",
                 _id: "1NF1N1T3"
             },
             {
                 title: "Keyboard Not Found",
                 severity: 3,
-                description:"des",
+                description: "des",
                 _id: "K3YB0RD"
             },
             {
                 title: "404 Coffee Not Found",
                 severity: 2,
-                description:"des",
+                description: "des",
                 _id: "C0FF33"
             },
             {
                 title: "Unexpected Response",
                 severity: 1,
-                description:"des",
+                description: "des",
                 _id: "G0053"
             }
         ]
